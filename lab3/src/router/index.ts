95350f6dd5a19f57fc4e2a906178a95fe540bbac
Saw <ehkalanephaung96@gmail.com>
2023-07-30 16:34:27 +0700

    import EventLayoutView

--- lab3/src/router/index.ts
+++ lab3/src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import EventDetailView from '../views/details/EventDetailView.vue'
 import NotFoundView from '../views/NotFoundView.vue'
+import EventLayoutView from '../views/details/EventLayoutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
