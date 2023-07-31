60e15ff694d70ce29973441b9706c976bdc392d8
Saw <ehkalanephaung96@gmail.com>
2023-07-31 23:50:33 +0700

    add a progress bar

--- lab3/src/router/index.ts
+++ lab3/src/router/index.ts
@@ -5,6 +5,7 @@ import EventDetailView from '../views/details/EventDetailView.vue'
 import EventLayoutView from '../views/details/EventLayoutView.vue'
 import EventAirlineView from '../views/details/EventAirlineView.vue'
 import NotFoundView from '../views/NotFoundView.vue'
+import NProgress from 'nprogress'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -54,6 +55,13 @@ const router = createRouter({
       props: true
     }
   ]
+})
+router.beforeEach(() => {
+  NProgress.start()
+})
+
+router.afterEach(() => {
+  NProgress.done()
 })
 
 export default router
