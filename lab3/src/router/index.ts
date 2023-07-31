5dc5192bce8e1574a5302eab59e26e094dd229d9
Saw <ehkalanephaung96@gmail.com>
2023-08-01 00:33:57 +0700

    Move the part of loading passenger details from the component

--- lab3/src/router/index.ts
+++ lab3/src/router/index.ts
@@ -6,10 +6,43 @@ import EventLayoutView from '../views/details/EventLayoutView.vue'
 import EventAirlineView from '../views/details/EventAirlineView.vue'
 import NotFoundView from '../views/NotFoundView.vue'
 import NProgress from 'nprogress'
+import EventLayoutViewVue from '../views/details/EventLayoutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/details/:id', // Adjust the path as per your requirement
+      name: 'event-layout',
+      component: EventLayoutViewVue, // Replace this with the actual component
+      // Add the beforeEnter guard for the event-layout route
+      beforeEnter: (to, from, next) => {
+        const id: number = parseInt(to.params.id as string)
+        const eventStore = useEventStore()
+        const store = useMessageStore()
+
+        EventService.getEventById(id)
+          .then((response: AxiosResponse<CardItem>) => {
+            // Set the event data in the event store
+            eventStore.setEvent(response.data)
+
+            // Optionally, show a flash message indicating successful data retrieval
+            store.updateMessage('Passenger details loaded successfully.')
+            setTimeout(() => {
+              store.resetMessage()
+            }, 5000)
+
+            // Continue with the route navigation
+            next()
+          })
+          .catch(() => {
+            // Handle error if data retrieval fails
+            // Redirect to 404 or error page
+            next({ name: '404-resource', params: { resource: 'PassengerId' } })
+          })
+      }
+    },
+    // other route configurations...
     {
       path: '/',
       name: 'home',
